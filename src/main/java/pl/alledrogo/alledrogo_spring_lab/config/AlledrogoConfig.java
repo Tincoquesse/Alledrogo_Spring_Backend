9b9004e8727bf71cc13601a590c1d083c39f99de
Tincoquesse <kamil.sound@gmail.com>
2022-06-15 15:20:52 +0200

    CorsFilter for Angular port 4020 added

--- src/main/java/pl/alledrogo/alledrogo_spring_lab/config/AlledrogoConfig.java
+++ src/main/java/pl/alledrogo/alledrogo_spring_lab/config/AlledrogoConfig.java
@@ -2,9 +2,15 @@ package pl.alledrogo.alledrogo_spring_lab.config;
 
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import pl.alledrogo.alledrogo_spring_lab.controller.AlledrogoController;
+
+import java.util.Arrays;
 
 @Configuration
 public class AlledrogoConfig {
@@ -14,6 +20,19 @@ AlledrogoController controller;
 
     public AlledrogoConfig(AlledrogoController controller) {
         this.controller = controller;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedMethods(Arrays.asList(CorsConfiguration.ALL));
+        corsConfiguration.setAllowedHeaders(Arrays.asList(CorsConfiguration.ALL));
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
     }
 
     @EventListener(ApplicationReadyEvent.class)
