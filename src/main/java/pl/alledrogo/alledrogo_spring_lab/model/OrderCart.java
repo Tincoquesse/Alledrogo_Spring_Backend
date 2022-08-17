34e642420860729fe4beccfe28ee4e6bb99e5eb5
Tincoquesse <kamil.sound@gmail.com>
2022-08-17 14:37:14 +0200

    OrderCart.java added

---
+++ src/main/java/pl/alledrogo/alledrogo_spring_lab/model/OrderCart.java
@@ -0,0 +1,72 @@
+package pl.alledrogo.alledrogo_spring_lab.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class OrderCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany
+    private List<Product> products;
+
+    private String street;
+    private String postalCode;
+    private String city;
+    private Integer phoneNumber;
+
+    public OrderCart() {
+    }
+
+    public OrderCart(List<Product> products, String street, String postalCode, String city, Integer phoneNumber) {
+        this.products = products;
+
+        this.street = street;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
